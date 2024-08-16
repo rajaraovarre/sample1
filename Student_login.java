7a1affdffedf1f3eb0082f3a197f6dc9923a35a9
Raz <mrrajaraovarre143@gmail.com>
2024-08-16 12:09:30 +0000

    First commit

---
+++ Student_login.java
@@ -0,0 +1,13 @@
+
+
+public class Student_login
+{
+
+        public static void main(String args[])
+        {
+            System.out.println("Hai Varre");
+            System.out.println("Welcome to TCS");
+
+        }
+
+}
